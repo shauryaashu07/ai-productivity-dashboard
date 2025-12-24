aeed76d3632baefb8c867c213ba7a6d25e2df214
Shaurya Gupta <shauryagupta2017gmail@gmail.com>
2025-12-24 19:05:52 +0530

    Day 8: Initialize React frontend with Vite

---
+++ frontend-react/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ minHeight: "100vh", background: "#020617", color: "white" }}>
+      <h1 style={{ padding: "20px" }}>
+        React Day 8 – Setup Complete 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
